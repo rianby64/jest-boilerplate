7ec695e76f20c34b271962e28bd8d20855b0890d
Andres Rios <rianby64@gmail.com>
2019-06-01 01:02:34 +0300

    Do an small refactoring

--- src/app/Summ.tsx
+++ src/app/Summ.tsx
@@ -1,30 +1,35 @@
 
 import * as React from 'react';
 
+type SummType = React.ReactElement<SummProps>
+
 interface SummProps {
-    children?: React.ReactElement<SummProps> | React.ReactElement<SummProps>[];
+    children?: SummType | SummType[];
     str: string;
 }
 
+const castStr = (str: string): number => Number(str ? str : 0);
+const summStr = (k: number, str: string): string => (k + castStr(str)).toString();
+
 export const Summ =
-(props: SummProps): React.ReactElement<SummProps> => {
-    const k = Number(props.str ? props.str : 0);
+(props: SummProps): SummType => {
+    const k = castStr(props.str);
 
     if (Array.isArray(props.children)) {
         return (
             <div>
-                { props.children.map((child: React.ReactElement<SummProps>, key: number): React.ReactElement<SummProps> =>
-                    <Summ key={key.toString()} str={(Number(child.props.str) + k).toString()}>
+                { props.children.map((child: SummType, key: number): SummType =>
+                    <Summ key={key.toString()} str={summStr(k, child.props.str)}>
                         { child.props.children }
                     </Summ>)}
             </div>
         );
     }
-    const i = k + Number(props.children ? props.children.props.str : 0);
+    const i = summStr(k, props.children ? props.children.props.str : '0');
     return (
         <div>
             {props.children ?
-                <Summ str={i.toString()}>
+                <Summ str={i}>
                     { props.children.props.children }
                 </Summ>
                 : k}
