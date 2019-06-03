97f766be41deb3a82fe2b387b3b0af1a202dacc6
Andres Rios <rianby64@gmail.com>
2019-06-03 13:24:55 +0300

    Ok!

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -5,12 +5,12 @@ import { render } from 'react-dom';
 import { Summ } from './Summ';
 
 render(
-    <Summ str="1">
-        <Summ str="2">
-            <Summ str="3"></Summ>
+    <Summ v={1}>
+        <Summ v={2}>
+            <Summ v={3}></Summ>
         </Summ>
-        <Summ str="4"></Summ>
-        <Summ str="5"></Summ>
+        <Summ v={4}></Summ>
+        <Summ v={5}></Summ>
     </Summ>,
     document.getElementById("root")
 );
