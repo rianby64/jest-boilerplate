6c2b50eedaf1dde8b110417a61e977ab2d823b78
Andres Rios <rianby64@gmail.com>
2019-06-03 13:17:22 +0300

    Ok...

--- src/app/Summ.test.tsx
+++ src/app/Summ.test.tsx
@@ -4,83 +4,83 @@ import { mount } from 'enzyme';
 import { Summ } from './Summ';
 
 test('Summ exists', (): void => {
-    const el = (<Summ str=""></Summ>);
+    const el = (<Summ v={0}></Summ>);
     const sh = mount(el);
     expect(sh).toMatchSnapshot();
 });
 
 test('Summ accepts one parameter', (): void => {
-    const str = '2';
-    const el = (<Summ str={str}></Summ>);
+    const v = 2;
+    const el = (<Summ v={v}></Summ>);
     const sh = mount(el);
-    expect(sh.text()).toBe(str);
+    expect(sh.text()).toBe(v.toString());
     expect(sh).toMatchSnapshot();
 });
 
 test('Summ the parent with its soley child', (): void => {
-    const str1 = '2';
-    const str2 = '6';
-    const expected = (Number(str1) + Number(str2)).toString();
+    const v1 = 2;
+    const v2 = 6;
+    const expected = v1 + Number(v2);
     const el = (
-        <Summ str={str1}>
-            <Summ str={str2}></Summ>
+        <Summ v={v1}>
+            <Summ v={v2}></Summ>
         </Summ>
     );
     const sh = mount(el);
-    expect(sh.text()).toBe(expected);
+    expect(sh.text()).toBe(expected.toString());
     expect(sh).toMatchSnapshot();
 });
 
 test('Summ te parent with its two nested children', (): void => {
-    const str1 = '2';
-    const str2 = '6';
-    const str3 = '3';
-    const expected = (Number(str1) + Number(str2) + Number(str3)).toString();
+    const v1 = 2;
+    const v2 = 6;
+    const v3 = 3;
+    const expected = Number(v1) + Number(v2) + Number(v3);
     const el = (
-        <Summ str={str1}>
-            <Summ str={str2}>
-                <Summ str={str3}></Summ>
+        <Summ v={v1}>
+            <Summ v={v2}>
+                <Summ v={v3}></Summ>
             </Summ>
         </Summ>
     );
     const sh = mount(el);
-    expect(sh.text()).toBe(expected);
+    expect(sh.text()).toBe(expected.toString());
     expect(sh).toMatchSnapshot();
 });
 
 test('Summ the parent with its two children separately', (): void => {
-    const str1 = '1';
-    const str2 = '2';
-    const str3 = '3';
-    const expected = '34';
+    const v1 = 1;
+    const v2 = 2;
+    const v3 = 3;
+    const expected = 34;
     const el = (
-        <Summ str={str1}>
-            <Summ str={str2}></Summ>
-            <Summ str={str3}></Summ>
+        <Summ v={v1}>
+            <Summ v={v2}></Summ>
+            <Summ v={v3}></Summ>
         </Summ>
     );
     const sh = mount(el);
-    expect(sh.text()).toBe(expected);
+    expect(sh.text()).toBe(expected.toString());
     expect(sh).toMatchSnapshot();
 });
 
 test('Summ the parent with a complex tree of children', (): void => {
-    const str1 = '1';
-    const str2 = '2';
-    const str3 = '3';
-    const str4 = '4';
-    const str5 = '5';
-    const expected = '656';
+    const v1 = 1;
+    const v2 = 2;
+    const v3 = 3;
+    const v4 = 4;
+    const v5 = 5;
+    const expected = 656;
     const el = (
-        <Summ str={str1}>
-            <Summ str={str2}>
-                <Summ str={str3}></Summ>
+        <Summ v={v1}>
+            <Summ v={v2}>
+                <Summ v={v3}></Summ>
             </Summ>
-            <Summ str={str4}></Summ>
-            <Summ str={str5}></Summ>
+            <Summ v={v4}></Summ>
+            <Summ v={v5}></Summ>
         </Summ>
     );
     const sh = mount(el);
-    expect(sh.text()).toBe(expected);
+    expect(sh.text()).toBe(expected.toString());
     expect(sh).toMatchSnapshot();
 });
--- src/app/Summ.tsx
+++ src/app/Summ.tsx
@@ -5,31 +5,28 @@ type SummType = React.ReactElement<SummProps>
 
 interface SummProps {
     children?: SummType | SummType[];
-    str: string;
+    v: number;
 }
-
-const castStr = (str: string): number => Number(str ? str : 0);
-const summStr = (k: number, str: string): string => (k + castStr(str)).toString();
 
 export const Summ =
 (props: SummProps): SummType => {
-    const k = castStr(props.str);
+    const k = props.v;
 
     if (Array.isArray(props.children)) {
         return (
             <div>
                 { props.children.map((child: SummType, key: number): SummType =>
-                    <Summ key={key.toString()} str={summStr(k, child.props.str)}>
+                    <Summ key={key.toString()} v={k + child.props.v}>
                         { child.props.children }
                     </Summ>)}
             </div>
         );
     }
-    const i = summStr(k, props.children ? props.children.props.str : '0');
+    const i = k + (props.children ? props.children.props.v : 0);
     return (
         <div>
             {props.children ?
-                <Summ str={i}>
+                <Summ v={i}>
                     { props.children.props.children }
                 </Summ>
                 : k}
